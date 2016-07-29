b5205e89a64faba27bb8f42a0e72d0d7ec01a0f7
wenmagi <958425607@qq.com>
2016-07-29 13:09:34 +0800

    ww

--- app/src/main/java/com/wen/magi/baseframe/activities/MainActivity.java
+++ app/src/main/java/com/wen/magi/baseframe/activities/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         mainTv.setOnClickListener(this);
         mainTv1.setOnClickListener(this);
-        LogUtils.e("wwwwwwwwweeeee");
+        LogUtils.e("wwwwwwwwweeeeewww");
     }
 
     @Override
